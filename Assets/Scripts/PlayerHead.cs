20602cf02d48689e4de1ac98aedca6b1827d9a5f
File_Missing <michaverheij@gmail.com>
2025-02-12 17:09:01 +0100

    added small polishing to end sequence

--- Assets/Scripts/PlayerHead.cs
+++ Assets/Scripts/PlayerHead.cs
@@ -5,6 +5,8 @@ public class PlayerHead : MonoBehaviour
     [SerializeField] float scaleFactor = 0.1f;
     void Update()
     {
+        if (GameManager.instance.gameEnded) return;
+
         transform.localScale = Vector3.one + Vector3.one * ((GameManager.instance.headSize - 1) * scaleFactor);
     }
 }
